9edf3cbca21afe85bbe6a9589b1ee5a882c73402
Kyle Allan <kallan@riotgames.com>
2012-07-06 11:55:21 -0700

    adding short mappings for tasks. be more DRY with set_remote_configuration

--- lib/nexus_cli/tasks.rb
+++ lib/nexus_cli/tasks.rb
@@ -6,10 +6,22 @@ module NexusCli
       base.send :include, ::Thor::Actions
       base.class_eval do
 
+        map 'pull'   => :pull_artifact
+        map 'push'   => :push_artifact
+        map 'info'   => :get_artifact_info
+        map 'custom' => :get_artifact_custom_info
+        map 'config' => :get_nexus_configuration
+        map 'status' => :get_nexus_status
+
         class_option :overrides,
           :type => :hash,
           :default => {},
           :desc => "A hashed list of overrides. Available options are 'url', 'repository', 'username', and 'password'."
+
+        def initialize(*args)
+          super
+          set_remote_configuration(options[:overrides])
+        end
 
         method_option :destination, 
           :type => :string,
@@ -18,7 +30,6 @@ module NexusCli
         desc "pull_artifact artifact", "Pulls an artifact from Nexus and places it on your machine."
         def pull_artifact(artifact)
           begin
-            set_remote_configuration(options[:overrides])
             path_to_artifact = Remote.pull_artifact(artifact, options[:destination], options[:overrides])
             say "Artifact has been retrived and can be found at path: #{path_to_artifact}", :green
           rescue NexusCliError => e
@@ -34,7 +45,6 @@ module NexusCli
         desc "push_artifact artifact file", "Pushes an artifact from your machine onto the Nexus."
         def push_artifact(artifact, file)
           begin
-            set_remote_configuration(options[:overrides])
             Remote.push_artifact(artifact, file, options[:insecure], options[:overrides])
             say "Artifact #{artifact} has been successfully pushed to Nexus.", :green
           rescue NexusCliError => e
@@ -46,7 +56,6 @@ module NexusCli
         desc "get_artifact_info artifact", "Gets and returns the metadata in XML format about a particular artifact."
         def get_artifact_info(artifact)
           begin
-            set_remote_configuration(options[:overrides])
             say Remote.get_artifact_info(artifact, options[:overrides]), :green
           rescue NexusCliError => e
             say e.message, :red
@@ -57,7 +66,6 @@ module NexusCli
         desc "get_artifact_custom_info artifact", "Gets and returns the custom metadata in XML format about a particular artifact."
         def get_artifact_custom_info(artifact)
           begin
-            set_remote_configuration(options[:overrides])
             say Remote.get_artifact_custom_info(artifact, options[:overrides]), :green
           rescue NexusCliError => e
             say e.message, :red
@@ -68,7 +76,6 @@ module NexusCli
         desc "get_nexus_configuration", "Prints out configuration from the .nexus_cli file that helps inform where artifacts will be uploaded."
         def get_nexus_configuration
           begin
-            set_remote_configuration(options[:overrides])
             config = Remote.configuration
             say "********* Reading CLI configuration from #{File.expand_path('~/.nexus_cli')} *********", :blue
             say "Nexus URL: #{config['url']}", :blue
@@ -82,7 +89,6 @@ module NexusCli
         desc "get_nexus_status", "Prints out information about the Nexus instance."
         def get_nexus_status
           begin
-            set_remote_configuration(options[:overrides])
             data = Remote.status
             say "********* Getting Nexus status from #{data['base_url']} *********", :blue
             say "Application Name: #{data['app_name']}", :blue
