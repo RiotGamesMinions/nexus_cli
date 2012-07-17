51ae74be6fdf9249e041db5aaac95faf3bd5ca56
Kyle Allan <kallan@riotgames.com>
2012-07-16 18:25:09 -0700

    starting from scratch on tests. Beginning work on get_nexus_status.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,15 +1,8 @@
 require 'aruba/cucumber'
-
 $:.push "#{File.dirname(__FILE__)}/../../lib/"
 require 'nexus_cli'
 require 'rspec'
 
-After do |scenario|
-  FileUtils.rm_f("mytar-1.0.3.tgz")
-
-  tmp_path = File.join(ENV["TMPDIR"], "mytar-1.0.3.tgz")
-  FileUtils.rm_f(tmp_path)
-
-  NexusCli::Remote.delete_artifact('com.foo.bar:myFile:1.0.0:tgz')
-  FileUtils.rm_f("myFile.tgz")
+def get_overrides
+  @overrides ||= {:url => 'http://localhost:8081/nexus', :repository => 'releases', :username => 'deployment', :password => 'deployment123'}
 end
