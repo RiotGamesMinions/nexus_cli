8d15611563858521ec15e512e99c74df7718fa33
Mario Vuong <mvuong@riotgames.com>
2012-09-19 17:31:16 -0700

    Cleaned up more custom metadata helper code
    Cleaned up some method naming
    Added more documentation

--- lib/nexus_cli/n3_metadata.rb
+++ lib/nexus_cli/n3_metadata.rb
@@ -3,23 +3,30 @@ require 'nokogiri'
 module NexusCli
   module N3Metadata
     class << self
+      # Checks if the custom metadata key is valid.
+      # Valid characters are alphanumeric with no special characeters.
       def valid_n3_key?(element)
         return !element.match(/^[a-zA-Z0-9]+$/).nil? ? true : false
       end
 
+      # Checks if the custom metadata value is valid.
+      # Valid characters are anything but quotes.
       def valid_n3_value?(element)
         return !element.match(/^[^"'\\]*$/).nil? ? true : false
       end
 
+      # Check if the custom metadata search type is valid.
       def valid_n3_search_type?(element)
         return ["equal", "notequal", "matches", "bounded"].include?(element)
       end
 
-      def create_custom_metadata_subject(group_id, artifact_id, version, extension)
+      # Creates a custom metadata subject for HTTP requests.
+      def create_subject(group_id, artifact_id, version, extension)
         return "urn:maven/artifact##{group_id}:#{artifact_id}:#{version}::#{extension}"
       end
 
-      def parse_request_into_simple(custom_metadata)
+      # Parses the regular custom metadata xml into a simpler format containing only the custom metadata.
+      def convert_result_to_simple_xml(custom_metadata)
         request = []
         Nokogiri::XML(custom_metadata).root.search("//customMetadataResponse/data/customMetadata[namespace=\"urn:nexus/user#\"]").each do |row|
           request.push(create_tag(row.at("key").text.strip, row.at("value").text.strip))
@@ -27,7 +34,8 @@ module NexusCli
         return Nokogiri::XML("<artifact-resolution><data>#{request.join}</data></artifact-resolution>").root.to_xml(:indent => 4)
       end
 
-      def parse_request_into_hash(custom_metadata)
+      # Parses the regular custom metadata xml into a hash containing only the custom metadata.
+      def convert_result_to_hash(custom_metadata)
         request = {}
         Nokogiri::XML(custom_metadata).root.search("//customMetadataResponse/data/customMetadata[namespace=\"urn:nexus/user#\"]").each do |row|
           request[row.at("key").text.strip] = row.at("value").text.strip
@@ -35,7 +43,12 @@ module NexusCli
         return request
       end
 
-      def generate_request_from_hash(source, target={})
+      # Create the request from the specified list of custom metadata key:value pairs
+      # @info If the target hash contains empty values for a key that exist in source, the metadata will be deleted
+      # @param [Hash] source The source hash of custom metadata key:value pairs
+      # @param [Hash] target The target hash to merge with the source hash (optional)
+      # @result [Hash] The resulting merge of the source and target hashes
+      def create_metadata_hash(source, target={})
         request = []
         source.merge(target).each do |key, value|
           request.push({:namespace => "urn:nexus/user#", :key => key, :value => value, :readOnly => false}) unless value.empty?
