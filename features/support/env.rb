42d880c3534362d6a44fcce1feb8ee7d368d9490
Jamie Winsor <jamie@vialstudios.com>
2012-11-11 14:57:11 -0800

    add guard to project for faster testing

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,6 @@
-require 'aruba/cucumber'
-$:.push "#{File.dirname(__FILE__)}/../../lib/"
-require 'nexus_cli'
-require 'rspec'
+require 'rubygems'
+require 'bundler'
+require 'spork'
 
 module ArubaOverrides
   def detect_ruby(cmd)
@@ -14,42 +13,52 @@ module ArubaOverrides
   end
 end
 
-World(ArubaOverrides)
+Spork.prefork do
+  require 'aruba/cucumber'
+  require 'nexus_cli'
+  require 'rspec'
 
-Before do
-  @aruba_timeout_seconds = 10
-end
+  World(ArubaOverrides)
 
-def get_overrides_string
-  @overrides_string ||= "url:http://localhost:8081/nexus repository:releases username:admin password:admin123"
-end
-
-def get_overrides
-  @overrides ||= {'url' => 'http://localhost:8081/nexus', 'repository' => 'releases', 'username' => 'admin', 'password' => 'admin123'}
-end
-
-def create_step_overrides(overrides)
-  overrides_hash = overrides.split(" ").inject({}) do |overrides_hash, override|
-    key, value = override.split(":")
-    overrides_hash[key] = value
-    overrides_hash
+  Before do
+    @aruba_timeout_seconds = 10
   end
 
-  step_overrides = get_overrides.merge(overrides_hash)
-  step_overrides.to_a.inject("") do |overrides_string, pair|
-    overrides_string << pair.join(":")
-    overrides_string << " "
+  def get_overrides_string
+    @overrides_string ||= "url:http://localhost:8081/nexus repository:releases username:admin password:admin123"
+  end
+
+  def get_overrides
+    @overrides ||= {'url' => 'http://localhost:8081/nexus', 'repository' => 'releases', 'username' => 'admin', 'password' => 'admin123'}
+  end
+
+  def create_step_overrides(overrides)
+    overrides_hash = overrides.split(" ").inject({}) do |overrides_hash, override|
+      key, value = override.split(":")
+      overrides_hash[key] = value
+      overrides_hash
+    end
+
+    step_overrides = get_overrides.merge(overrides_hash)
+    step_overrides.to_a.inject("") do |overrides_string, pair|
+      overrides_string << pair.join(":")
+      overrides_string << " "
+    end
+  end
+
+  def temp_dir
+    @tmpdir ||= Dir.mktmpdir
+  end
+
+  def nexus_remote
+    @nexus_remote ||= NexusCli::RemoteFactory.create(get_overrides)
+  end
+
+  at_exit do
+    FileUtils.rm_rf(temp_dir)
   end
 end
 
-def temp_dir
-  @tmpdir ||= Dir.mktmpdir
-end
-
-def nexus_remote
-  @nexus_remote ||= NexusCli::RemoteFactory.create(get_overrides)
-end
-
-at_exit do
-  FileUtils.rm_rf(temp_dir)
+Spork.each_run do
+  require 'nexus_cli'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
-APP_ROOT = File.expand_path('../../', __FILE__)
+require 'rubygems'
+require 'bundler'
+require 'spork'
 
-ENV["NEXUS_CONFIG"] = File.join(APP_ROOT, "spec", "fixtures", "nexus.config")
+Spork.prefork do
+  require 'webmock/rspec'
 
-require 'nexus_cli'
-require 'webmock/rspec'
+  APP_ROOT = File.expand_path('../../', __FILE__)
+  ENV["NEXUS_CONFIG"] = File.join(APP_ROOT, "spec", "fixtures", "nexus.config")
+end
+
+Spork.each_run do
+  require 'nexus_cli'
+end
