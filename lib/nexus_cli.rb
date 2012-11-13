4e1aaabed6f160648ed8f3707cc0ab1582f2571e
Kyle Allan <kallan@riotgames.com>
2012-11-12 17:01:12 -0800

    move sanitize_for_id and parse_artifact_string to NexusCli module

--- lib/nexus_cli.rb
+++ lib/nexus_cli.rb
@@ -89,6 +89,32 @@ module NexusCli
     end
   end
 
+  # Transforms a given [String] into a sanitized version by
+  # replacing spaces with underscores and downcasing.
+  # 
+  # @param  unsanitized_string [String] the String to sanitize
+  # 
+  # @return [String] the sanitized String
+  def sanitize_for_id(unsanitized_string)
+    unsanitized_string.gsub(" ", "_").downcase
+  end
+
+  # Parses a given artifact string into its
+  # four, distinct, Maven pieces.
+  # 
+  # @param  artifact [String] the Maven identifier
+  # 
+  # @return [Array<String>] an Array with four elements
+  def parse_artifact_string(artifact)
+    split_artifact = artifact.split(":")
+    if(split_artifact.size < 4)
+      raise ArtifactMalformedException
+    end
+    group_id, artifact_id, version, extension = split_artifact
+    version.upcase! if version.casecmp("latest")
+    return group_id, artifact_id, version, extension
+  end
+
   # Determines whether or not the Nexus server being
   # connected to is running Nexus Pro.
   def running_nexus_pro?
--- lib/nexus_cli/oss_remote.rb
+++ lib/nexus_cli/oss_remote.rb
@@ -16,33 +16,5 @@ module NexusCli
       @configuration = Configuration::parse(overrides)
       @ssl_verify = ssl_verify
     end
-
-    private
-
-    # Transforms a given [String] into a sanitized version by
-    # replacing spaces with underscores and downcasing.
-    # 
-    # @param  unsanitized_string [String] the String to sanitize
-    # 
-    # @return [String] the sanitized String
-    def sanitize_for_id(unsanitized_string)
-      unsanitized_string.gsub(" ", "_").downcase
-    end
-
-    # Parses a given artifact string into its
-    # four, distinct, Maven pieces.
-    # 
-    # @param  artifact [String] the Maven identifier
-    # 
-    # @return [Array<String>] an Array with four elements
-    def parse_artifact_string(artifact)
-      split_artifact = artifact.split(":")
-      if(split_artifact.size < 4)
-        raise ArtifactMalformedException
-      end
-      group_id, artifact_id, version, extension = split_artifact
-      version.upcase! if version.casecmp("latest")
-      return group_id, artifact_id, version, extension
-    end
   end
 end
