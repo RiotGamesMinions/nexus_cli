86aecc241620b3ecc67c35904427c15beaca44f1
Kyle Allan <kallan@riotgames.com>
2013-04-17 17:39:18 -0700

    refactor configuration class and test it better

--- lib/nexus_cli/configuration.rb
+++ lib/nexus_cli/configuration.rb
@@ -5,39 +5,41 @@ module NexusCli
     DEFAULT_FILE = "~/.nexus_cli".freeze
 
     class << self
-      attr_writer :path
-
       # The filepath to the nexus cli configuration file
       #
       # @return [String]
-      def path
-        @path || File.expand_path(ENV['NEXUS_CONFIG'] || DEFAULT_FILE)
+      def file_path
+        File.expand_path(ENV['NEXUS_CONFIG'] || File.expand_path(DEFAULT_FILE))
       end
 
       # @param [Hash] overrides
       #
       # @return [Hash]
-      def parse(overrides = {})
-        config = File.exists?(self.path) ? YAML::load_file(self.path) : Hash.new
+      def from_overrides(overrides)
+        raise MissingSettingsFileException unless overrides
 
-        if config.nil? && (overrides.nil? || overrides.empty?)
-          raise MissingSettingsFileException
-        end
+        validate_config(overrides)
+        sanitize_config(overrides)
+      end
 
-        unless overrides.nil? || overrides.empty?
-          overrides.each { |key, value| config[key] = value }
-        end
+      def from_file
+        config = YAML.load_file(file_path)
+
+        raise MissingSettingsFileException unless config
 
         validate_config(config)
-
-        config["repository"] = config["repository"].gsub(" ", "_").downcase
-        config
+        sanitize_config(config)
       end
 
       def validate_config(configuration)
         ["url", "repository", "username", "password"].each do |key|
           raise InvalidSettingsException.new(key) if configuration[key].blank?
         end
+      end
+
+      def sanitize_config(config)
+        config["repository"] = config["repository"].gsub(" ", "_").downcase
+        config.symbolize_keys
       end
     end
   end
--- spec/unit/nexus_cli/configuration_spec.rb
+++ spec/unit/nexus_cli/configuration_spec.rb
@@ -1,23 +1,123 @@
-require 'nexus_cli'
+require 'spec_helper'
 
-describe NexusCli do
-  it "gives you errors when configuration has a blank password" do
-    overrides = {"url" => "http://somewebsite.com", "username" => "admin", "password" => ""}
-    expect {NexusCli::Configuration.parse(overrides)}.to raise_error(NexusCli::InvalidSettingsException)
+describe NexusCli::Configuration do
+  subject { configuration }
+  let(:configuration) { described_class }
+  let(:valid_config) {
+      {
+        "url" => "http://somewebsite.com",
+        "repository" => "foo",
+        "username" => "admin",
+        "password" => "password"
+      }
+    }
+
+  describe "::from_overrides" do
+    subject { from_overrides }
+    let(:from_overrides) { configuration.from_overrides(valid_config) }
+
+    it "returns a hash" do
+      from_overrides.should be_a(Hash)
+    end
   end
 
-  it "gives you errors when configuration has a blank username" do
-    overrides = {"url" => "http://somewebsite.com", "username" => "", "password" => "admin"}
-    expect {NexusCli::Configuration.parse(overrides)}.to raise_error(NexusCli::InvalidSettingsException)
+  describe "::from_file" do
+    subject { from_file }
+    let(:from_file) { configuration.from_file }
+
+    before do
+      YAML.stub(:load_file).and_return(valid_config)
+    end
+
+    context "when the NEXUS_CONFIG environment variable exists" do
+      let(:nexus_config_path) { "/home/var/nexus_cli" }
+
+      before do
+        ENV['NEXUS_CONFIG'] = nexus_config_path
+      end
+
+      it "loads the config file from NEXUS_CONFIG" do
+        YAML.should_receive(:load_file).with(nexus_config_path)
+        from_file
+      end
+    end
+
+    context "when the NEXUS_CONFIG environment variable does not exist" do
+      let(:nexus_config_path) { File.expand_path(NexusCli::Configuration::DEFAULT_FILE) }
+
+      before do
+        ENV['NEXUS_CONFIG'] = nil
+      end
+
+      it "loads the config file from DEFAULT_FILE" do
+        YAML.should_receive(:load_file).with(nexus_config_path)
+        from_file
+      end
+    end
   end
 
-  it "gives you errors when configuration has a blank url" do
-    overrides = {"url" => "", "username" => "admin", "password" => "admin"}
-    expect {NexusCli::Configuration.parse(overrides)}.to raise_error(NexusCli::InvalidSettingsException)
+  describe "::validate_config" do
+    subject { validate_config }
+    let(:validate_config) { described_class.validate_config(config) }
+
+    context "when a password is missing" do
+      let(:config) {  valid_config.slice("url", "repository", "username") }
+
+      it "raises an InvalidSettingsException" do
+        expect { validate_config }.to raise_error(NexusCli::InvalidSettingsException)
+      end
+    end
+
+    context "when a username is missing" do
+      let(:config) {  valid_config.slice("url", "repository", "password") }
+
+      it "raises an InvalidSettingsException" do
+        expect { validate_config }.to raise_error(NexusCli::InvalidSettingsException)
+      end
+
+    end
+
+    context "when a url is missing" do
+      let(:config) {  valid_config.slice("password", "repository", "username") }
+
+      it "raises an InvalidSettingsException" do
+        expect { validate_config }.to raise_error(NexusCli::InvalidSettingsException)
+      end
+    end
+
+    context "when a repository is missing" do
+      let(:config) {  valid_config.slice("url", "password", "username") }
+
+      it "raises an InvalidSettingsException" do
+        expect { validate_config }.to raise_error(NexusCli::InvalidSettingsException)
+      end
+    end
   end
 
-  it "gives you errors when configuration has a blank repository" do
-    overrides = {"url" => "http://somewebsite.com", "repository" => ""}
-    expect {NexusCli::Configuration.parse(overrides)}.to raise_error(NexusCli::InvalidSettingsException)
+  describe "::sanitize_config" do
+    subject { sanitize_config }
+    let(:sanitize_config) { described_class.sanitize_config(valid_config) }
+
+    context "when the repository has spaces" do
+      let(:valid_config) {
+        {
+          "url" => "http://somewebsite.com",
+          "repository" => "foo bar",
+          "username" => "admin",
+          "password" => "password"
+        }
+      }
+
+      it "turns them into underscores" do
+        sanitize_config[:repository].should eq("foo_bar")
+        sanitize_config
+      end
+    end
+
+    it "symbolizes the keys" do
+      sanitize_config.each do |key, value|
+        key.should be_a(Symbol)
+      end
+    end
   end
 end
