d0d6480231c5e01f7b46a7cc05d4966598e696b3
Kyle Allan <kallan@riotgames.com>
2013-08-21 17:22:30 -0700

    rename gav to coordinates

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -18,29 +18,29 @@ When /^I call the nexus "(.*?)" command as the "(.*?)" user with password "(.*?)
   step "I run `nexus-cli #{command} --overrides=#{overrides}`"
 end
 
-When /^I push an artifact with the GAV of "(.*)"$/ do |gav|
-  groupId, artifact_id, extension, version = gav.split(":")
+When /^I push an artifact with the GAV of "(.*)"$/ do |coordinates|
+  groupId, artifact_id, extension, version = coordinates.split(":")
   file = File.new(File.join(temp_dir, "#{artifact_id}-#{version}.#{extension}"), 'w')
   file.puts "some data"
   file.close
-  step "I run `nexus-cli push #{gav} #{file.path} --overrides=#{get_overrides_string}`"
+  step "I run `nexus-cli push #{coordinates} #{file.path} --overrides=#{get_overrides_string}`"
 end
 
-When /^I pull an artifact with the GAV of "(.*?)" to a temp directory$/ do |gav|
-  step "I run `nexus-cli pull #{gav} --destination #{temp_dir} --overrides=#{get_overrides_string}`"
+When /^I pull an artifact with the GAV of "(.*?)" to a temp directory$/ do |coordinates|
+  step "I run `nexus-cli pull #{coordinates} --destination #{temp_dir} --overrides=#{get_overrides_string}`"
 end
 
 Then /^I should have a copy of the "(.*?)" artifact in a temp directory$/ do |fileName|
   File.exists?(File.join(temp_dir, fileName)).should == true
 end
 
-When /^I delete an artifact with the GAV of "(.*)"$/ do |gav|
-  nexus_remote.delete_artifact(gav)
+When /^I delete an artifact with the GAV of "(.*)"$/ do |coordinates|
+  nexus_remote.delete_artifact(coordinates)
 end
 
-When /^I delete an artifact with the GAV of "(.*?)" from the "(.*?)" repository$/ do |gav, repository|
+When /^I delete an artifact with the GAV of "(.*?)" from the "(.*?)" repository$/ do |coordinates, repository|
   nexus_remote.configuration["repository"] = repository
-  nexus_remote.delete_artifact(gav)
+  nexus_remote.delete_artifact(coordinates)
 end
 
 When /^I edit the "(.*?)" files "(.*?)" field to true$/ do |file, field|
